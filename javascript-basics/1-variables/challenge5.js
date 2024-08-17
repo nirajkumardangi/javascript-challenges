a87d231dd54924032930d3ae79af3993b00399a7
Niraj Kumar Dangi <niraj8825224435@gmail.com>
2024-08-17 14:37:29 +0530

    challenge5 solved

---
+++ javascript-basics/1-variables/challenge5.js
@@ -0,0 +1,8 @@
+/* Create two variables, firstName and lastName. Assign your first name and last name to them. Then, create a new variable fullName by concatenating firstName and lastName with a space in between. Print fullName to the console. */
+
+// SOLUTION
+
+let firstName = 'niraj';
+let lastName = 'dangi';
+let fullName = firstName + ' ' + lastName;
+console.log(fullName); 
