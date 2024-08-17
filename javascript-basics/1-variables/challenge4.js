ad859dead7a21866f7dfa62062dc8c6404e85ced
Niraj Kumar Dangi <niraj8825224435@gmail.com>
2024-08-17 14:22:12 +0530

    challenge4 solved

---
+++ javascript-basics/1-variables/challenge4.js
@@ -0,0 +1,11 @@
+/* Below, we attempt to assign the value of a variable named numOne to the variable numTwo. But, that variable has not been declared yet. Declare a variable named numOne and run the code.
+reset
+
+let numTwo = numOne;
+console.log(numTwo); */
+
+
+// SOLUTION
+let numOne = 5;
+let numTwo = numOne;
+console.log(numTwo);
