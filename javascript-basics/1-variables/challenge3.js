91e8923dbe4ce1c5316fc7c8b41eb0219998403c
Niraj Kumar Dangi <niraj8825224435@gmail.com>
2024-08-17 14:15:16 +0530

    challenge3 solved

---
+++ javascript-basics/1-variables/challenge3.js
@@ -0,0 +1,12 @@
+/* Here, we have two variables numOne and numTwo. numOne already has a value. Assign numTwo the value of numOne and run the code.
+
+let numOne = 5;
+let numTwo;
+console.log(numTwo); */
+
+
+// SOLUTION
+
+let numOne = 5;
+let numTwo = numOne;
+console.log(numTwo);
