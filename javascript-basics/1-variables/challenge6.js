13b7c8a781b9a23d1f7a45194facb3db62787686
Niraj Kumar Dangi <niraj8825224435@gmail.com>
2024-08-17 14:40:48 +0530

    challenge6 solved

---
+++ javascript-basics/1-variables/challenge6.js
@@ -0,0 +1,11 @@
+/* Challenge 6: Arithmetic Operation with Variables
+Task: Declare two variables, a and b, and assign them the values 10 and 5 respectively. Create a new variable sum that stores the sum of a and b. Print the value of sum to the console. */
+
+// SOLUTION
+
+let a = 10;
+let b = 5;
+let sum = a + b;
+console.log(sum);  
+
+// OUTPUT: 15
