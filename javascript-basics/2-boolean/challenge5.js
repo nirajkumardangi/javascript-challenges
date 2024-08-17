2a0b84204e5effaf596a047b0e4c443d43ea3d29
Niraj Kumar Dangi <niraj8825224435@gmail.com>
2024-08-17 15:15:48 +0530

    challenge5 solved

---
+++ javascript-basics/2-boolean/challenge5.js
@@ -0,0 +1,10 @@
+/* Challenge 5: Boolean Coercion
+Task: Declare a variable userInput and assign it an empty string. Create a variable hasInput that coerces userInput into a boolean value to check if it is truthy or falsy. Print hasInput to the console. */
+
+// SOLUTION
+
+let userInput = "";
+let hasInput = Boolean(userInput);
+console.log(hasInput);
+
+// OUTPUT: false
