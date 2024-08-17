a725e9656a57b6cf3dea974d3f1ecd378142d2d9
Niraj Kumar Dangi <niraj8825224435@gmail.com>
2024-08-17 14:14:33 +0530

    challenge2 solved

---
+++ javascript-basics/1-variables/challenge2.js
@@ -0,0 +1,11 @@
+/* Here, we declare the variable num. But, it has no value yet. Assign a value to it and run the code.
+
+let num;
+
+console.log(num); */
+
+// SOLUTION
+
+let num;
+num = 2;
+console.log(num);
