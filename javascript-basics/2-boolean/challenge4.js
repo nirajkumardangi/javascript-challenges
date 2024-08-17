1cc416762a3e250593cdedcba608d5828b3b8ed4
Niraj Kumar Dangi <niraj8825224435@gmail.com>
2024-08-17 15:14:09 +0530

    challenge4 solved

---
+++ javascript-basics/2-boolean/challenge4.js
@@ -0,0 +1,10 @@
+/* Challenge 6: Boolean from Comparison Expressions
+Task: Create a variable temperature and assign it a value. Create a new variable isCold that is true if the temperature is less than 15 degrees. Print isCold to the console. */
+
+// SOLUTION
+
+let temperature = 10;
+let isCold = temperature < 15;
+console.log(isCold);
+
+// OUTPUT: true;
