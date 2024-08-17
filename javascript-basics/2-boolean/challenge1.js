592c5438226a93a17cc5a1b0f3986abd4f0707fa
Niraj Kumar Dangi <niraj8825224435@gmail.com>
2024-08-17 15:03:49 +0530

    challenge1 solved

---
+++ javascript-basics/2-boolean/challenge1.js
@@ -0,0 +1,7 @@
+/* Challenge 1: Simple Boolean Declaration
+Task: Declare a variable isAdult and assign it a boolean value based on whether a person is 18 years or older. Print the value of isAdult to the console. */
+
+// SOLUTION
+
+let isAdult = 18 >= 18;
+console.log(isAdult);  // OUTPUT: true
