e13ac371bdf245d1ddce8742fe17f0b06c1db904
Niraj Kumar Dangi <niraj8825224435@gmail.com>
2024-08-17 15:07:11 +0530

    challenge2 solved

---
+++ javascript-basics/2-boolean/challenge2.js
@@ -0,0 +1,12 @@
+/* Challenge 2: Boolean Comparison
+Task: Create two variables a and b with values 10 and 20 respectively. Create a new variable isEqual that stores the result of the comparison between a and b to check if they are equal. Print isEqual to the console. */
+
+
+// SOLUTION
+
+let a = 10;
+let b = 20;
+let isEqual = a === b;
+console.log(isEqual);
+
+// OUTPUT: false
