52ba04f50a405956c5d6c08fadfe3e7d03a26a90
Niraj Kumar Dangi <niraj8825224435@gmail.com>
2024-08-17 15:11:08 +0530

    challenge3 solved

---
+++ javascript-basics/2-boolean/challenge3.js
@@ -0,0 +1,11 @@
+/* Challenge 3: Logical AND Operator
+Task: Declare two boolean variables isSunny and isWeekend. Use the logical AND operator to create a new variable goForWalk that is true only if both isSunny and isWeekend are true. Print goForWalk to the console. */
+
+// SOLUTION
+
+let isSunny = true;
+let isWeekend = true;
+let goForWalk = isSunny && isWeekend;
+console.log(goForWalk);
+
+// OUTPUT: true;
