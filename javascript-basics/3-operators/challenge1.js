170f1b1eee2e28794b304e726f2fc4122aae2a97
Niraj Kumar Dangi <niraj8825224435@gmail.com>
2024-08-17 15:39:18 +0530

    challenge1 solved

---
+++ javascript-basics/3-operators/challenge1.js
@@ -0,0 +1,11 @@
+/* Challenge 1: Addition Operator
+Task: Create two variables, a and b, with values 5 and 10 respectively. Use the addition operator to find their sum and store it in a variable sum. Print sum to the console. */
+
+// SOLUTION
+
+let a = 5;
+let b = 10;
+let sum = a + b;
+console.log(sum);
+
+// OUTPUT: 15
