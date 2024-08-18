5a378a3df936224c4ea2be76c2ddd0a1fdb88296
Niraj Kumar Dangi <niraj8825224435@gmail.com>
2024-08-18 15:45:08 +0530

    challenge7 solved

---
+++ javascript-basics/3-operators/challenge7.js
@@ -0,0 +1,16 @@
+/* Challenge 7: Increment Operator
+Task: Create a variable count and initialize it with the value 0. Use the increment operator to increase count by 1 three times. Print count after each increment. */
+
+// SOLUTION
+
+let count = 0;
+console.log(count)
+count++;
+console.log(count)
+count++;
+console.log(count)
+
+// OUTPUT: 
+// 0
+// 1
+// 2
