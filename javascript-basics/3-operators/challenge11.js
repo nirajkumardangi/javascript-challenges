a30e781cde59aff6be5b918e5b6819e3c43197eb
Niraj Kumar Dangi <niraj8825224435@gmail.com>
2024-08-18 15:52:55 +0530

    challenge11 solved

---
+++ javascript-basics/3-operators/challenge11.js
@@ -0,0 +1,12 @@
+/* Challenge 11: Comparison Operator (Equality)
+Task: Create two variables, a and b, with values 10 and "10" respectively. Use the equality operator to check if they are equal and store the result in isEqual. Print isEqual to the console. */
+
+// SOLUTION
+
+let a = 10;
+let b = '10';
+
+let isEqual = a == b; 
+console.log(isEqual);
+
+// OUTPUT: true
