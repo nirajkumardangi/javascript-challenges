a9913fafe3fee45909d10d5021b161ebd4ffcb08
Niraj Kumar Dangi <niraj8825224435@gmail.com>
2024-08-18 15:53:42 +0530

    challenge12 solved

---
+++ javascript-basics/3-operators/challenge12.js
@@ -0,0 +1,12 @@
+/* Challenge 12: Strict Equality Operator
+Task: Using the same variables a and b from the previous challenge, use the strict equality operator to check if they are strictly equal. Store the result in isStrictEqual and print it. */
+
+// SOLUTION
+
+let a = 10;
+let b = '10';
+
+let isEqual = a === b;
+console.log(isEqual);
+
+// OUTPUT: false
