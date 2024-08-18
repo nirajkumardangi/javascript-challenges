e42feead02e5913f8972709891ed0a3a1bc14cc0
Niraj Kumar Dangi <niraj8825224435@gmail.com>
2024-08-18 15:48:47 +0530

    challenge9 solved

---
+++ javascript-basics/3-operators/challenge9.js
@@ -0,0 +1,11 @@
+/* Challenge 9: Compound Assignment with Addition
+Task: Create a variable total and initialize it with the value 50. Use the compound assignment operator for addition to add 25 to total. Print total. */
+
+// SOLUTION
+
+let total = 50;
+total += 25;
+
+console.log(total);
+
+// OUTPUT: 75
