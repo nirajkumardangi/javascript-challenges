3ae129de19d08613b69942c9ede097221892baaa
Niraj Kumar Dangi <niraj8825224435@gmail.com>
2024-08-18 15:46:59 +0530

    challenge8 solved

---
+++ javascript-basics/3-operators/challenge8.js
@@ -0,0 +1,16 @@
+/* Challenge 8: Decrement Operator
+Task: Create a variable countDown and initialize it with the value 10. Use the decrement operator to decrease countDown by 1 twice. Print countDown after each decrement. */
+
+// SOLUTION
+
+let countDown = 10;
+countDown--;
+countDown--;
+console.log(countDown);
+countDown--;
+countDown--;
+console.log(countDown);
+
+// OUTPUT: 
+// 8
+// 6
