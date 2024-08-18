d6b6d0f3bc5b32ddd2af391ffac82e757e62854b
Niraj Kumar Dangi <niraj8825224435@gmail.com>
2024-08-18 15:38:56 +0530

    challenge4 solved

---
+++ javascript-basics/3-operators/challenge4.js
@@ -0,0 +1,12 @@
+/* Challenge 4: Division Operator
+Task: Create two variables, p and q, with values 16 and 4 respectively. Use the division operator to find the quotient and store it in a variable quotient. Print quotient to the console. */
+
+// SOLUTION
+
+let p = 20;
+let q = 8;
+
+let quotient = p / q;
+console.log(quotient);
+
+// OUTPUT: 2.5
