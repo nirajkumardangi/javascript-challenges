fe4249fcefb438a1331a334da4f651b70a8763e0
Niraj Kumar Dangi <niraj8825224435@gmail.com>
2024-08-18 15:40:06 +0530

    challenge5 solved

---
+++ javascript-basics/3-operators/challenge5.js
@@ -0,0 +1,12 @@
+/* Challenge 5: Modulus Operator
+Task: Create two variables, num1 and num2, with values 29 and 5 respectively. Use the modulus operator to find the remainder and store it in a variable remainder. Print remainder to the console. */
+
+// SOLUTION
+
+let num1 =29;
+let num2 = 5;
+
+let remainder = num1 % num2;
+console.log(remainder);
+
+// OUTPUT: 4
