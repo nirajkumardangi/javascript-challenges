d69164a6e127951324bc1df39af61a3cca032425
Niraj Kumar Dangi <niraj8825224435@gmail.com>
2024-08-18 15:41:14 +0530

    challenge6 solved

---
+++ javascript-basics/3-operators/challenge6.js
@@ -0,0 +1,12 @@
+/* Challenge 6: Exponentiation Operator
+Task: Create two variables, base and exponent, with values 3 and 4 respectively. Use the exponentiation operator to raise base to the power of exponent and store it in a variable result. Print result to the console. */
+
+// SOLUTION
+
+let num1 = 2;
+let num2 = 3;
+
+let power = num1 ** num2;
+console.log(power);
+
+// OUTPUT: 8
