9a59c63c17f7e41daffb27d284bfc0bfe6f3c490
Niraj Kumar Dangi <niraj8825224435@gmail.com>
2024-08-18 15:54:53 +0530

    challenge13 solved

---
+++ javascript-basics/3-operators/challenge13.js
@@ -0,0 +1,12 @@
+/* Challenge 13: Greater Than Operator
+Task: Create two variables, age1 and age2, with values 18 and 21 respectively. Use the greater than operator to check if age2 is greater than age1. Print the result.*/
+
+
+// SOLUTION
+
+let age1 = 18;
+let age2 = 21;
+
+console.log(age1 > age2);
+
+// OUTPUT: false
