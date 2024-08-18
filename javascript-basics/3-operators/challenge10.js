3f0b91db8f32ec8286fe0999374af0bc1b4a02fd
Niraj Kumar Dangi <niraj8825224435@gmail.com>
2024-08-18 15:51:10 +0530

    challenge10 solved

---
+++ javascript-basics/3-operators/challenge10.js
@@ -0,0 +1,11 @@
+/* Challenge 10: Compound Assignment with Subtraction
+Task: Create a variable balance and initialize it with the value 100. Use the compound assignment operator for subtraction to subtract 30 from balance. Print balance. */
+
+// SOLUTION
+
+let balance = 100;
+balance -= 30;
+
+console.log(balance);
+
+// OUTPUT: 70
