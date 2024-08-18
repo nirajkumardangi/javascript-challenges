4722c38db516696286960e49281d3b3864538eb9
Niraj Kumar Dangi <niraj8825224435@gmail.com>
2024-08-18 15:56:04 +0530

    challenge14 solved

---
+++ javascript-basics/3-operators/challenge14.js
@@ -0,0 +1,11 @@
+/* Challenge 14: Less Than Operator
+Task: Create two variables, height1 and height2, with values 150 and 160 respectively. Use the less than operator to check if height1 is less than height2. Print the result. */
+
+// SOLUTION
+
+let height1 = 150;
+let height2 = 160;
+
+console.log(height1 < height2);
+
+// OUTPUT: true
