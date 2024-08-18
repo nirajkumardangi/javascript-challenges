f6a16ed195d03f5582eeaf1bf810346039bf246d
Niraj Kumar Dangi <niraj8825224435@gmail.com>
2024-08-18 15:36:35 +0530

    challenge2 solved

---
+++ javascript-basics/3-operators/challenge2.js
@@ -0,0 +1,12 @@
+/* Challenge 2: Subtraction Operator
+Task: Create two variables, x and y, with values 20 and 8 respectively. Use the subtraction operator to find their difference and store it in a variable difference. Print difference to the console. */
+
+// SOLUTION
+
+let x = 20;
+let y = 8;
+
+let difference = x -y;
+console.log(difference);
+
+// OUTPUT: 
