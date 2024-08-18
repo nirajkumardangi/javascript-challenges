a52a96bdcd4b65c76dbb084e3a40e80747dc3b2f
Niraj Kumar Dangi <niraj8825224435@gmail.com>
2024-08-18 15:38:03 +0530

    challenge3 solved

---
+++ javascript-basics/3-operators/challenge3.js
@@ -0,0 +1,12 @@
+/* Challenge 3: Multiplication Operator
+Task: Create two variables, m and n, with values 4 and 7 respectively. Use the multiplication operator to find their product and store it in a variable product. Print product to the console. */
+
+// SOLUTION
+
+let m = 20;
+let n = 8;
+
+let product = m * n;
+console.log(product);
+
+// OUTPUT: 160
