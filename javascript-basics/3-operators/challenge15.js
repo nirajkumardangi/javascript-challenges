fd996c26d5eaf8261b53f866f08839b61f7a385c
Niraj Kumar Dangi <niraj8825224435@gmail.com>
2024-08-18 15:58:00 +0530

    challenge15 solved

---
+++ javascript-basics/3-operators/challenge15.js
@@ -0,0 +1,12 @@
+/* Challenge 15: Logical AND with Comparison Operators
+Task: Create three variables, num1, num2, and num3 with values 10, 15, and 20 respectively. Use comparison operators along with the logical AND operator to check if num1 is less than num2 and num2 is less than num3. Print the result. */
+
+// SOLUTION
+
+let num1 = 10
+let num2 = 15
+let num3 = 30
+
+console.log((num1 < num2) && (num2 < num3));
+
+// OUTPUT: true
